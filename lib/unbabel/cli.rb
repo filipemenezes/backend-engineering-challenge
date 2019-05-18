0ecc947ea27453c187cdfbee6cd686052ad6de8c
Filipe Menezes <filipepmenezes@gmail.com>
2019-05-17 21:23:02 -0300

    Initial commit

---
+++ lib/unbabel/cli.rb
@@ -0,0 +1,8 @@
+require "unbabel/cli/version"
+
+module Unbabel
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/unbabel/cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Unbabel::Cli do
+  it "has a version number" do
+    expect(Unbabel::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
