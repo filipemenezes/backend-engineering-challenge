0825b41fbb5a408bba87da73e73e95fcfb4cb40d
Filipe Menezes <filipepmenezes@gmail.com>
2019-05-20 22:57:26 -0300

    Implement Application#average method

--- lib/unbabel/cli.rb
+++ lib/unbabel/cli.rb
@@ -5,6 +5,8 @@ require 'active_support/all'
 require 'unbabel/cli/version'
 require 'unbabel/cli/application'
 require 'unbabel/cli/input_stream'
+require 'unbabel/cli/output_stream'
+require 'unbabel/cli/reporter'
 require 'unbabel/cli/moving_average_calculator'
 
 module Unbabel
--- lib/unbabel/cli/application.rb
+++ lib/unbabel/cli/application.rb
@@ -7,8 +7,20 @@ module Unbabel
     class Application < Thor
       desc 'average [SOURCE_FILE]', 'Calculate moving average of SOURCE_FILE'
       option :window_size, type: :numeric, default: 10
+      option :output_file, type: :string
       def average(source_file)
-        # TODO
+        input_stream = Unbabel::CLI::InputStream.new(source_file)
+
+        output_data = Unbabel::CLI::MovingAverageCalculator.new(
+          input_stream.read,
+          options[:window_size]
+        ).calculate
+
+        if options[:output_file]
+          Unbabel::CLI::OutputStream.new(output_data, options[:output_file]).write
+        else
+          Unbabel::CLI::Reporter.new(output_data).run
+        end
       end
     end
   end
